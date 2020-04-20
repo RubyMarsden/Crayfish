537b66a06acd172f79fbee83a6022c538a0dd169
RubyMarsden <connemara.marsden@gmail.com>
2020-04-20 18:50:50 +0800

    More work on sbm time series widget

---
+++ src/utils/ui_utils.py
@@ -0,0 +1,17 @@
+from PyQt5.QtWidgets import QVBoxLayout, QWidget
+from matplotlib.backends.backend_qt5agg import FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+
+
+def create_figure_widget(fig, parent):
+    canvas = FigureCanvas(fig)
+    toolbar = NavigationToolbar(canvas, parent)
+
+    layout = QVBoxLayout()
+    layout.setContentsMargins(0, 0, 0, 0)
+    layout.addWidget(canvas)
+    layout.addWidget(toolbar)
+
+    widget = QWidget()
+    widget.setLayout(layout)
+    return widget, canvas
--- src/views/sbm_time_series_dialog.py
+++ src/views/sbm_time_series_dialog.py
@@ -1,7 +1,8 @@
 import matplotlib
-from PyQt5.QtWidgets import QDialog, QVBoxLayout, QWidget, QHBoxLayout, QLabel
+from PyQt5.QtWidgets import QDialog, QVBoxLayout, QWidget, QHBoxLayout, QLabel, QPushButton
 from matplotlib.backend_bases import NavigationToolbar2
 
+from utils import ui_utils
 from views.sample_tree import SampleTreeWidget
 
 matplotlib.use('QT5Agg')
@@ -20,68 +21,65 @@ class SBMTimeSeriesDialog(QDialog):
         self.setWindowTitle("SBM time series")
         self.setMinimumWidth(450)
 
-        layout = QVBoxLayout()
-        layout.addWidget(self._create_top_widget())
-        layout.addWidget(self._create_bottom_widget())
+        layout = QHBoxLayout()
+        layout.addWidget(self._create_left_widget())
+        layout.addWidget(self._create_right_widget())
         self.setLayout(layout)
+        first_sample = self.sample_tree.topLevelItem(0)
+        first_spot = first_sample.child(0)
+        self.sample_tree.setCurrentItem(first_spot)
 
-    def _create_top_widget(self):
+    def _create_right_widget(self):
         self.sample_tree = SampleTreeWidget()
         self.sample_tree.set_samples(self.samples)
         self.sample_tree.currentItemChanged.connect(self.on_selected_sample_change)
 
-        layout = QHBoxLayout()
-        layout.addWidget(self._create_spot_sbm_graph())
+        self.buttons = self._create_next_and_back_buttons()
+
+        layout = QVBoxLayout()
         layout.addWidget(self.sample_tree)
+        layout.addWidget(self.buttons)
 
         widget = QWidget()
         widget.setLayout(layout)
 
         return widget
 
-    def _create_bottom_widget(self):
+    def _create_next_and_back_buttons(self):
+
+        self.next_item_button = QPushButton("Next")
+        self.next_item_button.clicked.connect(self.on_next_item_clicked)
+        self.back_item_button = QPushButton("Back")
+        self.back_item_button.clicked.connect(self.on_back_item_clicked)
+
+        layout = QHBoxLayout()
+        layout.addWidget(self.back_item_button)
+        layout.addWidget(self.next_item_button)
+
+        widget = QWidget()
+        widget.setLayout(layout)
+
+        return widget
+
+    def _create_left_widget(self):
         layout = QVBoxLayout()
-        layout.addWidget(QLabel("Title"))
-        layout.addWidget(self._create_all_sbm_graph())
+        layout.addWidget(QLabel("Secondary beam monitor"))
+        layout.addWidget(self._create_sbm_graphs())
 
         widget = QWidget()
         widget.setLayout(layout)
         return widget
 
-    def _create_spot_sbm_graph(self):
-        fig, self.axis = plt.subplots()
-        # plt.xlim(*self._default_xlim)
-        # plt.ylim(*self._default_ylim)
+    def _create_sbm_graphs(self):
 
-        # plot
-        self.spot_canvas = FigureCanvas(fig)
-        toolbar = CustomNavigationToolbar(self.spot_canvas, self)
-
-        layout = QVBoxLayout()
-        layout.setContentsMargins(0, 0, 0, 0)
-        layout.addWidget(self.spot_canvas)
-        layout.addWidget(toolbar)
-
-        widget = QWidget()
-        widget.setLayout(layout)
-        return widget
-
-    def _create_all_sbm_graph(self):
-        fig, self.all_axis = plt.subplots()
+        fig = plt.figure()
+        self.axis = fig.add_subplot(2, 1, 1)
+        self.all_axis = fig.add_subplot(2, 1, 2)
 
         self.create_all_sbm_time_series(self.samples, self.all_axis)
 
-        # plot
-        self.all_canvas = FigureCanvas(fig)
-        toolbar = CustomNavigationToolbar(self.all_canvas, self)
+        widget, self.canvas = ui_utils.create_figure_widget(fig, self)
 
-        layout = QVBoxLayout()
-        layout.setContentsMargins(0, 0, 0, 0)
-        layout.addWidget(self.all_canvas)
-        layout.addWidget(toolbar)
-
-        widget = QWidget()
-        widget.setLayout(layout)
         return widget
 
     #############
@@ -94,15 +92,21 @@ class SBMTimeSeriesDialog(QDialog):
         axis.spines['right'].set_visible(False)
         xs, ys = zip(*time_series)
         axis.plot(xs, ys)
-        axis.set_xlabel("Time (s)")
+        axis.set_xlabel("Relative spot time (s)")
         axis.set_ylabel("SBM (cps)")
         plt.tight_layout()
-        self.spot_canvas.draw()
+        self.canvas.draw()
 
     def on_selected_sample_change(self, current_tree_item, previous_tree_item):
         if current_tree_item is None:
             self.axis.clear()
             return
+
+        next_item = self.sample_tree.itemBelow(current_tree_item)
+        self.next_item_button.setDisabled(next_item is None)
+
+        previous_item = self.sample_tree.itemAbove(current_tree_item)
+        self.back_item_button.setDisabled(previous_item is None)
 
         if current_tree_item.is_sample:
             return
@@ -111,10 +115,13 @@ class SBMTimeSeriesDialog(QDialog):
 
         self.highlight_spot_sbm(current_tree_item.spot)
 
+
+
+
     def create_all_sbm_time_series(self, samples, axis):
         axis.spines['top'].set_visible(False)
         axis.spines['right'].set_visible(False)
-        axis.set_xlabel("Time (s)")
+        axis.set_xlabel("Time (h)")
         axis.set_ylabel("SBM (cps)")
         plt.tight_layout()
 
@@ -128,60 +135,31 @@ class SBMTimeSeriesDialog(QDialog):
         interspot_time = 100
         for spot in sorted_spots:
             xs, ys = zip(*spot.sbm_time_series)
-            shifted_xs = [x + time for x in xs]
-            axis.plot(shifted_xs, ys, color = 'b')
+            output_xs = [(x + time)/3600 for x in xs]
+            axis.plot(output_xs, ys, color = 'b')
             self.start_end_time[spot] = time, time + spot.count_time_duration
             time += spot.count_time_duration + interspot_time
 
-
-        axis.set_xlim(0, time)
+        axis.set_xlim(0, time/3600)
 
     def highlight_spot_sbm(self, spot):
         if self.highlight_area is not None:
             self.highlight_area.remove()
-        print(spot.name)
         start_time, end_time = self.start_end_time[spot]
-        self.highlight_area = self.all_axis.fill_betweenx([0, 300000], start_time, end_time, facecolor='#ff000080')
-        self.all_canvas.draw()
+        self.highlight_area = self.all_axis.fill_betweenx([0, 300000], start_time/3600, end_time/3600, facecolor='#ff000080')
+        self.canvas.draw()
 
-#############
-## Hacking ##
-#############
+    def on_next_item_clicked(self):
+        self.back_item_button.setEnabled(True)
+        current_item = self.sample_tree.currentItem()
+        next_item = self.sample_tree.itemBelow(current_item)
+        if current_item.is_sample and current_item.childCount() > 0:
+            next_item = current_item.child(0)
 
-def _repurpose_toolbar_buttons():
-    new_tool_items = []
+        self.sample_tree.setCurrentItem(next_item)
 
-    # take out back and forward items and saves them
-    back_item = None
-    forward_item = None
-    for item in NavigationToolbar2.toolitems:
-        if item[0] == "Back":
-            back_item = item
-        elif item[0] == "Forward":
-            forward_item = item
-        else:
-            new_tool_items.append(item)
-
-    # new method for back and forward items
-    if back_item is None or forward_item is None:
-        raise Exception("Matplotlib API has changed")
-
-    back_item = list(back_item)
-    back_item[3] = "previous_spot"
-    forward_item = list(forward_item)
-    forward_item[3] = "next_spot"
-
-    new_tool_items.extend([back_item, forward_item])
-
-    return new_tool_items
-
-
-class CustomNavigationToolbar(NavigationToolbar):
-    # Use the standard toolbar items + download button
-    toolitems = _repurpose_toolbar_buttons()
-
-    def previous_spot(self):
-        print("previous")
-
-    def next_spot(self):
-        print("next")
+    def on_back_item_clicked(self):
+        self.next_item_button.setEnabled(True)
+        current_item = self.sample_tree.currentItem()
+        previous_item = self.sample_tree.itemAbove(current_item)
+        self.sample_tree.setCurrentItem(previous_item)
