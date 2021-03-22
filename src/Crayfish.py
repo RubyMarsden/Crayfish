53354074214f5e9d584280ec56cf582192197828
RubyMarsden <connemara.marsden@gmail.com>
2021-03-22 10:47:56 +0800

    Remove spurious qtpy imports

--- src/Crayfish.py
+++ src/Crayfish.py
@@ -1,19 +1,21 @@
 import sys
 
-from PyQt5.QtWidgets import QMessageBox
-from qtpy import QtWidgets
+from PyQt5.QtWidgets import QMessageBox, QApplication
 
 from models.crayfish_model import CrayfishModel
 from views.crayfish_window import CrayfishWindow
 
+
 def set_except_hook(window):
     def except_hook(cls, exception, traceback):
         sys.__excepthook__(cls, exception, traceback)
-        QMessageBox.critical(window,"Error", str(exception))
+        QMessageBox.critical(window, "Error", str(exception))
+
     sys.excepthook = except_hook
 
+
 if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
+    app = QApplication(sys.argv)
     app.setStyle("Fusion")
 
     model = CrayfishModel()
